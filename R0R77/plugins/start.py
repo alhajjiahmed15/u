5a925039d63bd18560bc2becbaf7b7d1f102f010
alhajjiahmed15 <110048852+alhajjiahmed15@users.noreply.github.com>
2022-07-27 02:38:10 +0300

    Update start.py

--- R0R77/plugins/start.py
+++ R0R77/plugins/start.py
@@ -24,7 +24,7 @@ async def start(event):
              caption=PM_START_TEXT.format(event.sender.first_name), 
              buttons=[
         [Button.url("➕ اضغط هنا لأضافتي", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [Button.url("السورس", "https://github.com/R0R77/Telethon-Music")],
+        [Button.url("السورس", "https://github.com/alhajjiahmed15/u")],
         [Button.url("الدعم", f"https://t.me/{Config.SUPPORT}"), Button.url("القناة", f"https://t.me/{Config.CHANNEL}")],
         [Button.inline("الاوامر", data="help")]])
        return
